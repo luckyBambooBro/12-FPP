94c07d9b44e6cf6ce9b2909b6f2e692bd2a54ca7
luckyBambooBro <huypham008@hotmail.com>
2025-11-12 08:22:09 +1030

    completed obtained_filtered_schools() but identified problem with obtain_filter_choices(). can only select one choice. considering overhauling to allow 2 choices

--- filters.py
+++ filters.py
@@ -1,4 +1,4 @@
-import pyinputplus as pyip
+import pyinputplus as pyip, pprint
 
 def obtain_filter_choices():
     print("With My School Selector you can specify criteria to filter through our list of schools!")
@@ -7,9 +7,17 @@ def obtain_filter_choices():
     filter_choices = {}
     if response.lower().startswith("y"):
         years_taught = pyip.inputMenu(["Any", "Primary", "Secondary", "Combined"], "Which schooling level are you searching for\n",)
-        gender = pyip.inputMenu(["Any", "Boys", "Girls", "Co-ed"], "Which student gender school are you looking for?\n",)
+        #gender = pyip.inputChoice(["Any", "Boys", "Girls", "Co-ed"], "Which types of school will you consider?\n",)
+        
+        gender = pyip.inputChoice(
+        ["Any", "Boys", "Girls", "Co-ed"],
+        limit=3,
+        separator=',', # Specifies the delimiter for multiple choices
+        allowRegexes=[r'^\s*Any\s*$'] # Allows "Any" as a single choice to skip filtering
+    )
+
         type = pyip.inputMenu(["Any", "Public", "Private"], "Do you prefer a public or private school?\n",)
-        religious = pyip.inputYesNo("Do you have a religious preference for the school? (Y/N)\n")
+        religious = pyip.inputMenu(["Any", "Religious", "Non-religious"], "Do you have a religious preference for the school?\n")
         preschool = pyip.inputYesNo("Are you looking for a school that includes a preschool? (Y/N)\n")
         osch = pyip.inputYesNo("Are you looking for OSHC? (Y/N)\n")
 
@@ -19,8 +27,12 @@ def obtain_filter_choices():
             filter_choices["gender"] = gender.lower()
         if not type == "Any":
             filter_choices["type"] = type.lower()
-        if religious == "yes":        
+
+        if religious == "Religious":        
             filter_choices["religious"] = True 
+        elif religious == "Non-religious":
+            filter_choices["religious"] = False
+
         if preschool == "yes":    
             filter_choices["preschool"] = True
         if osch == "yes":
@@ -34,7 +46,25 @@ def obtain_filter_choices():
     return filter_choices
 
 def obtain_filtered_schools(filter_choices, schools_data):
-    pass
+    print(f"FILTER CHOICES = {filter_choices}")
+    
+    filtered_school_data = schools_data
+    for filter_keys, filter_values in filter_choices.items():
+        filtered_school_data = [school for school in filtered_school_data if school[filter_keys] == filter_values]
+    pprint.pprint(filtered_school_data)
+
+
+
+
+
+    
+    
+    
+    
+    """ THE FOLLOWING WORKS BUT LIST COMP BETTER
+    for filter_keys, filter_values in filter_choices.items():
+        filtered_school_data = list(filter(lambda school_dict: school_dict[filter_keys] == filter_values, schools_data))
+    print(filtered_school_data)"""
 
 
     
---
+++ test.py
@@ -0,0 +1,2 @@
+#import pyinputplus as pyip, pprint
+

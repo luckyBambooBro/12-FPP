89cc43d0cc86f92bec714d23b0601c01020a2478
Huy Pham <huypham@Adas-MacBook-Pro.local>
2025-11-13 16:29:55 +1030

    need to fix obtain_filtered_schools

--- filters.py
+++ filters.py
@@ -28,7 +28,8 @@ def obtain_filter_choices():
             inquirer.Checkbox(
                 "type",
                 message="Do you prefer a public or private school?",
-                choices=["Any", "Public", "Private"]
+                choices=["Any", "Public", "Private"],
+                validate=validation_function_one_answer
             ),
             inquirer.Checkbox(
                 "religious",
@@ -50,13 +51,22 @@ def obtain_filter_choices():
             )    
         ]
         answers = inquirer.prompt(questions)
-        pprint.pprint(answers)
+        pprint.pprint(answers) #TODO uncomment this
+    return answers
 
 
 
 
 def obtain_filtered_schools(filter_choices, schools_data):
-    pass
+    """the following is incorrect and needs go be fixed
+    
+    unfiltered_schools_data = schools_data
+    for filter_key, filter_value in filter_choices.items():
+        if "Any" in filter_value:
+            filtered_schools_data = [school for school in unfiltered_schools_data ]         
+        else:
+            filtered_schools_data = [school for school in unfiltered_schools_data if school[filter_key] in filter_value]
+    print(filtered_schools_data)"""
 
 
 
